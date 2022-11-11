71bea80ca7153e348f87a61aad8a55b6c7301394
Marina Zalozna <marina.zalozna@gmail.com>
2022-11-11 13:01:38 +0100

    HW

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+// Получаем ссылки на кнопки
+const btnStartRef = document.querySelector('[data-start]');
+const btnStopRef = document.querySelector('[data-stop]');
 
+// Вводим переменную для получения id созданного таймером
+let idInterval = null;
+
+// Функция, которая вызывается при клике по кнопке Start
+const onChangeColor = (event) => {
+    onBtnDisabled ()
+    idInterval = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.backgroundColor =`${color}`;
+    }, 1000)
+}
+
+// Функция, которая вызывается при клике по кнопке Stop
+const onStopChangeColor = (event) => {
+    clearInterval(idInterval);
+    onBtnDisabled ()
+}
+// Функция для генерации случайного цвета 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// Функция для смены статуса кнопок (disabled) с true на false
+function onBtnDisabled () {
+  if (!btnStartRef.disabled) {
+    btnStartRef.disabled = true;
+    btnStopRef.disabled = false
+  } else {
+    btnStartRef.disabled = false;
+    btnStopRef.disabled = true
+  }
+}
+
+// Вешаем слушателей на кнопки
+btnStartRef.addEventListener("click", onChangeColor);
+btnStopRef.addEventListener("click", onStopChangeColor);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// Получаем ссылку на форму (чтобы повесить слушателя)
+const formRef = document.querySelector('.form');
+
+// Функция для создания промиса, принимает два параметра: номер создаваемого промиса (position) и задержку (delay)
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay })
+      }
+      reject({ position, delay })
+    }, delay);
+  });
+};
+  
+// Функция-колбек вызываемая при событии submit
+function onSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const dataForm = new FormData(form);
+  const finalData = {};
+  for (const [key, value] of dataForm.entries()) {
+    finalData[key] = Number(value);
   }
-}
+  // очищаем форму
+  form.reset();
+  // в цикле for вызываем функцию создающую промис
+  for (let position = 1; position <= finalData.amount; position += 1){
+    createPromise(position, finalData.delay).then(onSuccess).catch(onError);
+    finalData.delay = finalData.delay + finalData.step;
+  };
+};
+
+// Функция вызываемая методом catch, когда промис возвращает reject
+function onError({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+};
+
+// Функция вызываемая методом then, когда промис возвращает resolve
+function onSuccess({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+};
+
+// Вешаем слушателя на форму по событию submit
+formRef.addEventListener('submit', onSubmit);
